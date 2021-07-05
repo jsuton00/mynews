a7e6e389d70b44cb3ca63886bda9f9b603cfbbce
Jozo Suton <61884667+jsuton00@users.noreply.github.com>
2021-07-06 00:12:12 +0200

    news category navigation fixed

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -3,8 +3,13 @@ import { Link } from 'react-router-dom';
 import { NEWS_CATEGORIES_LINKS } from '../constants/newsCategories';
 
 const NavMenu = (props) => {
-	const { closeMenu } = props;
+	const { isOpen, closeMenu } = props;
 
+	const handleClick = () => {
+		if (isOpen === true) {
+			closeMenu();
+		}
+	};
 	return (
 		<nav className="nav news-category-nav">
 			{NEWS_CATEGORIES_LINKS.length > 0 &&
@@ -13,7 +18,7 @@ const NavMenu = (props) => {
 						<Link
 							key={index}
 							to={category.slug === '' ? '/' : `/news/${category.slug}`}
-							onClick={() => closeMenu()}
+							onClick={handleClick}
 							className="news-category-nav-link"
 						>
 							<div className="news-category-nav-link-item">
